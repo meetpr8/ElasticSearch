6b5112ac32568a48b53a56bd180c2170e6b244cd
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-19 15:18:08 +0530

    Update MovieRepo.java

--- src/main/java/com/example/demo/api/repo/MovieRepo.java
+++ src/main/java/com/example/demo/api/repo/MovieRepo.java
@@ -5,8 +5,7 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 import java.util.List;
 
-/*This is the main repo that is extending ElasticsearchRepository and our Custom Repository. In the configuration file, we
- have enabled this repo and hence we can use it as a bean */
+/*This is the main repo that is extending ElasticsearchRepository and our Custom Repository. */
 public interface MovieRepo extends ElasticsearchRepository<movies, String>,CustomRepo{
 
     public List<movies> findByTitle(String title);
