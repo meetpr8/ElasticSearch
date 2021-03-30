757f99e9aee5976b01e2389bf7a9dc9ac8701d1f
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-30 22:44:59 +0530

    Update RottenTomatoes.java

--- src/main/java/com/example/demo/api/model/RottenTomatoes.java
+++ src/main/java/com/example/demo/api/model/RottenTomatoes.java
@@ -1,32 +1,15 @@
 package com.example.demo.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Date;
 
 @Document(indexName = "movies")
+@Getter @Setter
 public class RottenTomatoes {
     private Date lastUpdated;
-
     private ViewerRating viewer;
 
-    public RottenTomatoes() {
-        super();
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public ViewerRating getViewer() {
-        return viewer;
-    }
-
-    public void setViewer(ViewerRating viewer) {
-        this.viewer = viewer;
-    }
 }
