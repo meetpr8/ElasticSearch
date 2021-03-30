0ba32168e5b3ef87d1aeda1347f07e6e987586aa
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-30 22:45:29 +0530

    Update ViewerRating.java

--- src/main/java/com/example/demo/api/model/ViewerRating.java
+++ src/main/java/com/example/demo/api/model/ViewerRating.java
@@ -1,30 +1,13 @@
 package com.example.demo.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "movies")
+@Getter @Setter
 public class ViewerRating {
     private double rating;
     private int numReviews;
-
-    public ViewerRating() {
-        super();
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public int getNumReviews() {
-        return numReviews;
-    }
-
-    public void setNumReviews(int numReviews) {
-        this.numReviews = numReviews;
-    }
 
 }
